d1b83d0075fc5bec16e757282c89e238c2d071d7
queen-cece <ccezeonu3@gmail.com>
2018-07-26 17:40:36 +0000

    automatically backed up by learn

---
+++ strings.rb
@@ -0,0 +1,11 @@
+# print "here is a string"
+# print "1234567"
+# print "**** there is another string ****"
+
+# puts "here is a string"
+# puts "1234567"
+# puts "**** there is another string****"
+
+puts "Cece Ezeonu"
+puts "Durham, North Carolina"
+puts "ccezeonu3@gmail.com"
--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,16 +1,26 @@
 
 def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
+  summer_olympics = {:Sydney =>"2000", :Athens =>"2004", :Beijing =>"2008", :London =>"2012"}
+  return summer_olympics
 end
 
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+  summer_olympics = create_olympics_hash
+  new_place = :Atlanta
+  new_year = "1996"
+  summer_olympics [new_place]=new_year
+  summer_olympics
 end
 
 def iterate_through_hash
-  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+  summer_olympics = add_a_key_value_pair
+  summer_olympics.do |key,value|
+    puts "The #{key} summer olympics took place in #{value}"
+  end
 end
+  
 
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
